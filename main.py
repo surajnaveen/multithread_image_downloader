1e62d9633d9b200702b2b1d4a74913e6f039e2d3
surajnaveen <suraj0nb@gmail.com>
2025-08-29 22:24:21 +0530

    Create main file and basic code, add req: file

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+def image_url(count):
+    if count<=0:
+        print("Invalid Number")
+        return
+    
+    for i in range(count):
+        url = f"https://picsum.photos/id/{i}/200/300"
+        yield url
+
+for url in image_url(10):
+    print(url)
